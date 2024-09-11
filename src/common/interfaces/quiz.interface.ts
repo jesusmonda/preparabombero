b2211f5b17120c29d2c290410a53a7fdbe8a43fc
Jesús Monda <jmonda@odilo.us>
2024-09-12 01:45:03 +0200

    fix pdf_name type

--- src/common/interfaces/quiz.interface.ts
+++ src/common/interfaces/quiz.interface.ts
@@ -1,4 +1,4 @@
 import { Quiz } from "@prisma/client";
 
-export type QuizOmitResult = Omit<Quiz, "result">;
+export type QuizOmitResult = Omit<Quiz, "result" | "pdf_name">;
 export type QuizAndStatus = Quiz & {status: string}
