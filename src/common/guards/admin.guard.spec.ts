45575c24a1ebde3bbadb8208cbfd5540aa1695f3
Jesús Monda <jmonda@odilo.us>
2024-09-12 02:08:34 +0200

    fix production

--- src/common/guards/admin.guard.spec.ts
+++ src/common/guards/admin.guard.spec.ts
@@ -2,6 +2,6 @@ import { AdminGuard } from './admin.guard';
 
 describe('AdminGuard', () => {
   it('should be defined', () => {
-    expect(new AdminGuard()).toBeDefined();
+    expect(new AdminGuard(null)).toBeDefined();
   });
 });
--- src/common/guards/user.guard.spec.ts
+++ src/common/guards/user.guard.spec.ts
@@ -2,6 +2,6 @@ import { UserGuard } from './user.guard';
 
 describe('UserGuard', () => {
   it('should be defined', () => {
-    expect(new UserGuard()).toBeDefined();
+    expect(new UserGuard(null)).toBeDefined();
   });
 });
