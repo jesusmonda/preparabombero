38b015251adc466974804c154dcbb75e05f930cd
jmonda <jmonda@K313.local>
2024-12-12 22:40:13 +0100

    fix order pdf

--- src/modules/pdf/pdf.service.ts
+++ src/modules/pdf/pdf.service.ts
@@ -9,9 +9,9 @@ export class PdfService {
   async findAll(sort: string): Promise<Pdf[]> {
     const query: {city?: 'desc', community?: 'desc'} = {}
     if (sort == 'community') {
-      query.city = 'desc';
-    } else {
       query.community = 'desc';
+    } else {
+      query.city = 'desc';
     }
     return await this.prisma.pdf.findMany({
       orderBy: query
